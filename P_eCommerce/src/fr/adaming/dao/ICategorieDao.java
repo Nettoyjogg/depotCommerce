783ac666dddeb689f00488034869469d54515bed
IN-BR-006 <IN-BR-006@IN-BR-006>
2019-03-12 14:26:54 +0100

    Test

--- P_eCommerce/src/fr/adaming/dao/ICategorieDao.java
+++ P_eCommerce/src/fr/adaming/dao/ICategorieDao.java
@@ -1,5 +1,15 @@
 package fr.adaming.dao;
 
+import fr.adaming.model.Categorie;
+
 public interface ICategorieDao {
+	
+	public Categorie ajouterCategorieDao(Categorie ca, Administrateur admin);
+			
+	public Categorie modifierCategorieDao(Categorie ca, Administrateur admin);
+	
+	public int supprimerCategorieDao(Categorie ca, Administrateur admin);
+	
+	public Categorie consulterCategorieDao(Categorie ca, Administrateur admin);
 
 }
