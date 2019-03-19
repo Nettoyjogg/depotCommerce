3693b87c68b565e4f19afc37bc4f6451afae89c3
IN-BR-010 <IN-BR-010@IN-BR-010.home>
2019-03-19 19:11:07 +0100

    sry

--- P_eCommerce/src/fr/adaming/dao/IProduitDao.java
+++ P_eCommerce/src/fr/adaming/dao/IProduitDao.java
@@ -2,10 +2,11 @@ package fr.adaming.dao;
 
 import java.util.List;
 
+import javax.ejb.Local;
 
 import fr.adaming.model.Produit;
 
-
+@Local
 public interface IProduitDao {
 	
 	public List<Produit> afficherProduitDao();
